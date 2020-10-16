7d74e2cd0761544d6f371a67f090e32baa77f8de
Ted M. Young <tedyoung@gmail.com>
2020-10-16 01:26:34 -0700

    Refactored to remove unnecessary isEmpty field.

--- src/main/java/com/jitterted/ebp/blackjack/Wallet.java
+++ src/main/java/com/jitterted/ebp/blackjack/Wallet.java
@@ -2,18 +2,15 @@ package com.jitterted.ebp.blackjack;
 
 public class Wallet {
   private int balance = 0;
-  private boolean isEmpty;
 
   public Wallet() {
-    isEmpty = true;
   }
 
   public boolean isEmpty() {
-    return isEmpty;
+    return balance == 0;
   }
 
   public void addMoney(int amount) {
-    isEmpty = false;
     balance += amount;
   }
 
